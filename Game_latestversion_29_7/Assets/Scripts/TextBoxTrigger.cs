b2fa2b23ec94975bebef8329242fff20febf5be2
unknown <oalva003@MDMA325W24.ksamk.ad>
2015-07-29 13:33:54 +0300

    New push. 29.7. 13:33

---
+++ Game_latestversion_29_7/Assets/Scripts/TextBoxTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBoxTrigger : MonoBehaviour {
+	public GameObject TextBox;
+
+	void Start () {
+		if (Application.loadedLevelName.Equals("introScene"))
+		{
+			showTextBox ();
+	}
+	}
+
+	public void showTextBox() {
+		
+		textBox.SetActive(true);
+		
+	}
+	
+	public void hideTextboxl() {
+		
+		TextBox.SetActive(false);
+		
+	}
+}
