a77a398a629d277f3a35e1ddd363c9a93ee242b7
unknown <oalva003@MDMA325W24.ksamk.ad>
2015-07-30 12:52:05 +0300

    Updated game with working TextBoxes on '0_menu' and '2_game'

--- Game_latestversion_29_7/Assets/Scripts/TextBoxTrigger.cs
+++ Game_latestversion_29_7/Assets/Scripts/TextBoxTrigger.cs
@@ -3,23 +3,27 @@ using System.Collections;
 
 public class TextBoxTrigger : MonoBehaviour {
 	public GameObject TextBox;
-
+	
 	void Start () {
-		if (Application.loadedLevelName.Equals("introScene"))
-		{
-			showTextBox ();
+		showTextBox ();		
 	}
-	}
-
+	
 	public void showTextBox() {
 		
-		textBox.SetActive(true);
+		TextBox.SetActive(true);
 		
 	}
 	
-	public void hideTextboxl() {
+	public void hideTextBox() {
 		
 		TextBox.SetActive(false);
 		
 	}
+	void Update () {
+		if (Input.GetKey (KeyCode.Return)) {
+			hideTextBox ();
+			
+		}
+	}
+	
 }
